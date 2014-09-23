8622c2f5c3a421cc4e6ffa3c2427c908b42aceb8
antoine <antoine@antoine-VB.(none)>
2014-09-23 14:42:26 -0400

    deadlock test,cannot write log

--- example/logfile/thread_test.c
+++ example/logfile/thread_test.c
@@ -16,23 +16,26 @@
 //#include <ipc.h>
 
 #define FILE_NAME "log"
+#define RUN_TIMES 100
 
-void *thread_func(void *ptr);
+void *thread_func1(void *ptr);
+void *thread_func2(void *ptr);
 char buf[256]={0};
 
 pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t buf_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main() {
 	pthread_t t1, t2;
 	int ret;
 
-	ret = pthread_create(&t1, NULL, &thread_func, NULL);
+	ret = pthread_create(&t1, NULL, &thread_func1, NULL);
 	if (ret != 0) {
 		printf("create thread1 failed\n");
 		return ret;
 	}
 
-	ret = pthread_create(&t2, NULL, &thread_func, NULL);
+	ret = pthread_create(&t2, NULL, &thread_func2, NULL);
 	if (ret != 0) {
 		printf("create thread2 failed\n");
 		return ret;
@@ -45,13 +48,30 @@ int main() {
 	exit(EXIT_SUCCESS);
 }
 
-void *thread_func(void *ptr) {
+void *thread_func1(void *ptr) {
 	int i;
-	for (i = 0; i < 5; i++) {
+	for (i = 0; i < RUN_TIMES; i++) {
 		pthread_mutex_lock(&log_mutex);
-		sprintf(buf, "thread %d write into logfile.\n", pthread_self());
+		usleep(1);
+		pthread_mutex_lock(&buf_mutex);
+		sprintf(buf, "thread %ld write into logfile.\n", pthread_self());
 		write_log_file(FILE_NAME, buf);
+		pthread_mutex_unlock(&buf_mutex);
 		pthread_mutex_unlock(&log_mutex);
-		usleep(100000 + (int) (100000.0 * rand() / RAND_MAX + 1));
+	//	usleep(1 + (int) (100000.0 * rand() / RAND_MAX + 1));
 	}
 }
+
+void *thread_func2(void *ptr) {
+        int i;
+        for (i = 0; i < RUN_TIMES; i++) {
+                pthread_mutex_lock(&buf_mutex);
+                pthread_mutex_lock(&log_mutex);                
+                sprintf(buf, "thread %ld write into logfile.\n", pthread_self());
+                write_log_file(FILE_NAME, buf);
+                pthread_mutex_unlock(&log_mutex);
+                pthread_mutex_unlock(&buf_mutex);
+          //      usleep(1 + (int) (10.0 * rand() / RAND_MAX + 1));
+        }
+}
+
